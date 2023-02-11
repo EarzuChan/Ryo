99c8ecd0d1a74389fc4e24d14a4c5c240ddda3dd
Earzu <huascq@163.com>
2023-02-11 16:49:21 +0800

    OK

--- Org.CNWeak.RyoReader/Program.cs
+++ Org.CNWeak.RyoReader/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ICSharpCode.SharpZipLib.Zip.Compression;
+using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
+using System;
+using System.Collections;
 using System.Diagnostics;
 using System.IO.Compression;
 using System.Linq;
@@ -76,7 +79,7 @@ namespace Org.CNWeak.RyoReader
             manager.RegDevCmds();
 #endif
 
-            manager.setLogListener(logListener);
+            manager.SetLogListener(logListener);
             StaticUtils.LogListener = logListener;
 
             if (args.Length > 0)
@@ -105,18 +108,18 @@ namespace Org.CNWeak.RyoReader
             {
                 LogListener = (str) => { Trace.WriteLine(str); };
 
-                commands.Add(new Command("Help", "获得软件命令帮助。"));
+                commands.Add(new Command("Help", "获得软件命令帮助"));
             }
 
             public void RegDefaultCmds()
             {
-                commands.Add(new Command("Load", "Load data from a file."));
+                commands.Add(new Command("Load", "从文件路径加载文件系统（*.fs）"));
             }
 
             public void RegDevCmds()
             {
-                commands.Add(new Command("Dump", "Dump data to a file."));
-                commands.Add(new Command("Pack", "Pack data into a compressed format."));
+                commands.Add(new Command("Dump", "转储数据到相同目录的Dump文件中"));
+                commands.Add(new Command("Pack", "Pack data into a compressed format"));
             }
 
             public void Load(string path)
@@ -127,8 +130,24 @@ namespace Org.CNWeak.RyoReader
                     if (stream.Length == 0) throw new Exception("文件长度为零");
 
                     var fileName = Path.GetFileNameWithoutExtension(stream.Name);
-                    LogListener("正在加载：" + fileName);
-                    new MassFile(fileName).Load(stream, "FileSystem");
+                    //LogListener("正在加载：" + fileName);
+
+                    var mass = new MassFile(fileName);
+
+                    LogListener($"加载文件：{fileName.ToUpper()}，稍后可凭借该名称转储相关数据");
+
+                    mass.Load(stream, "FileSystem");
+
+                    if (mass.ReadyToUse)
+                    {
+                        LogListener(fileName.ToUpper() + "的索引信息：");
+                        LogListener($"整数数组数据数：{mass.ObjCount}");
+                        LogListener($"G3L1数：{mass.G3L1}");
+                        LogListener($"类注册数：{mass.MyRegList.Count}");
+                        foreach (var item in mass.MyRegList) LogListener($"-- No.{item.Id} 类名：{item.ClazzName} 序列化器：{item.序列化器Name}");
+                        LogListener($"成员数：{mass.MyIdStrMap.Count}");
+                        foreach (var item in mass.MyIdStrMap) LogListener($"-- Id.{item.Value} Name：{item.Key}");
+                    }
                 }
                 catch (Exception e)
                 {
@@ -141,9 +160,14 @@ namespace Org.CNWeak.RyoReader
                 StaticUtils.PrintError(LogListener, v, e);
             }
 
-            public void Dump()
+            public void Dump(string str)
             {
-                LogListener("Dump data to a file.");
+                var mass = MassManager.MassList.Find((MassFile m) => m.Name.ToLower() == str.ToLower());
+                if (mass != null)
+                {
+                    LogListener($"找到文件：{str.ToUpper()}");
+                    LogListener(mass.DumpBuffer());
+                }
             }
 
             public void Pack()
@@ -198,15 +222,16 @@ namespace Org.CNWeak.RyoReader
                         if (cmdArgs.Length == cmd.Method.GetParameters().Length)
                         {
                             cmd.Method.Invoke(this, cmdArgs);
-                            return;
+                            //return;
                         }
                         else LogListener($"您的参数有误，{cmd.Name}的用法：{cmd.Help}");
+                        return;
                     }
                 }
                 LogListener("找不到命令，输入Help查看支持的命令。");
             }
 
-            public void setLogListener(Action<string> logListener)
+            public void SetLogListener(Action<string> logListener)
             {
                 LogListener = logListener;
             }
@@ -234,9 +259,25 @@ namespace Org.CNWeak.RyoReader
         public static List<MassFile> MassList = new();
     }
 
+    class RegItem
+    {
+        public string ClazzName { get; set; }
+        public string 序列化器Name { get; set; }
+        public int Id { get; set; }
+
+        public RegItem(int id, string str1, string str2)
+        {
+            Id = id;
+            ClazzName = str1;
+            序列化器Name = str2;
+        }
+    }
+
     class MassFile
     {
         public string Name = "";
+        public string FullPath = "";
+
         public InputStreamReader Reader = new(Stream.Null);
         public OutputStreamWriter Writer = new();
 
@@ -247,41 +288,55 @@ namespace Org.CNWeak.RyoReader
         public List<int> IntGroup3 = new();
         public List<int> IntGroup4 = new();
         public int G3L1 = 0;
-        public int RegCount = 0;
+        //public int RegCount = 0;
         public int KVCount = 0;
         public byte[] Buffer = Array.Empty<byte>();
         public Dictionary<string, int> MyIdStrMap = new();
+        public List<RegItem> MyRegList = new();
 
-        public bool ReadyToUse = false;
+        public bool ReadyToUse = true;
         public bool IsEmpty = true;
 
         public MassFile(string name)
         {
             Name = name;
+            MassManager.MassList.Add(this);
         }
 
         public void Clean()
         {
-            Reader = new(Stream.Null);
-            Writer = new();
-            ObjCount = 0;
-            CurrentObjCount = 0;
-            序列化了的类IdGroup = new();
-            索引对Group = new();
-            IntGroup3 = new();
-            IntGroup4 = new();
-            G3L1 = 0;
-            RegCount = 0;
-            KVCount = 0;
-            Buffer = Array.Empty<byte>();
+            ReadyToUse = false;
+            try
+            {
+                Reader = new(Stream.Null);
+                Writer = new();
+                ObjCount = 0;
+                CurrentObjCount = 0;
+                序列化了的类IdGroup = new();
+                索引对Group = new();
+                IntGroup3 = new();
+                IntGroup4 = new();
+                G3L1 = 0;
+                //RegCount = 0;
+                KVCount = 0;
+                Buffer = Array.Empty<byte>();
 
-            IsEmpty = true;
-            ReadyToUse = true;
+                IsEmpty = true;
+                ReadyToUse = true;
+            }
+            catch (Exception e)
+            {
+                StaticUtils.PrintError(null, "加载Mass失败", e);
+            }
         }
 
         public void Load(FileStream file, string format)
         {
+            FullPath = file.Name;
+
             if (!IsEmpty) Clean();
+
+            ReadyToUse = false;
             try
             {
                 Reader = new InputStreamReader(file);
@@ -294,22 +349,17 @@ namespace Org.CNWeak.RyoReader
 
                 if (inflated != null && inflated.Length != 0)
                 {
-                    // WRITE TO FILE
-                    /*var indexFile = new FileInfo("$newPath.index");
-                    if (!indexFile.Exists) indexFile.Create();
-                    File.WriteAllBytes(indexFile.FullName, inflated);*/
-
                     using var inflatedDataReader = new InputStreamReader(new MemoryStream(inflated));
                     ObjCount = inflatedDataReader.ReadInt();
-                    CurrentObjCount = inflatedDataReader.ReadInt();
+                    CurrentObjCount = ObjCount;
 
-                    for (var i = 0; i < ObjCount * 3; i++)
+                    for (var i = 0; i < ObjCount; i++)
                         序列化了的类IdGroup.Add(inflatedDataReader.ReadInt());
 
-                    for (var i = 0; i < ObjCount * 3; i++)
+                    for (var i = 0; i < ObjCount; i++)
                         索引对Group.Add(inflatedDataReader.ReadInt());
 
-                    for (var i = 0; i < ObjCount * 3; i++)
+                    for (var i = 0; i < ObjCount; i++)
                         IntGroup3.Add(inflatedDataReader.ReadInt());
 
                     G3L1 = IntGroup3.Last();
@@ -323,7 +373,7 @@ namespace Org.CNWeak.RyoReader
                         var id = inflatedDataReader.ReadInt();
                         var str1 = inflatedDataReader.ReadString();
                         var str2 = inflatedDataReader.ReadString();
-                        // REG
+                        MyRegList.Add(new RegItem(id, str1, str2));
                     }
 
                     var idStrMapCount = inflatedDataReader.ReadInt();
@@ -338,37 +388,74 @@ namespace Org.CNWeak.RyoReader
                 {
                     //NO INDEX INFO!
                 }*/
+                Buffer = Reader.ReadAllBytes();
+
+                ReadyToUse = true;
+                IsEmpty = false;
             }
             catch (Exception e)
             {
                 StaticUtils.PrintError(null, "加载Mass失败", e);
             }
         }
+
+        public string DumpBuffer()
+        {
+            if (!ReadyToUse || IsEmpty) return "未准备好或者内容为空";
+            if (String.IsNullOrWhiteSpace(FullPath)) return "保存路径为空";
+            var info = "保存";
+            try
+            {
+                using (FileStream fs = new FileStream(FullPath + ".dump", FileMode.Create, FileAccess.Write))
+                {
+                    fs.Write(Buffer, 0, Buffer.Length);
+                }
+                info += "成功，路径：" + FullPath + ".dump";
+            }
+            catch (Exception ex)
+            {
+                StaticUtils.PrintError(null, "保存时错误", ex);
+                info += "保存失败（悲";
+            }
+            return info;
+        }
     }
 
     class InputStreamReader : IDisposable
     {
-        private BinaryReader reader;
+        private BinaryReader Reader;
+        public long RestLength
+        {
+            get => Length - Position;
+        }
+        public long Length
+        {
+            get => Reader.BaseStream.Length;
+        }
+        public long Position
+        {
+            get => Reader.BaseStream.Position;
+        }
 
         public InputStreamReader(Stream inputStream)
         {
-            this.reader = new BinaryReader(inputStream);
+            this.Reader = new BinaryReader(inputStream);
         }
 
         public byte[] ReadBytes(int length)
         {
-            return reader.ReadBytes(length);
+            return Reader.ReadBytes(length);
         }
 
         public int ReadInt()
         {
-            int result = BitConverter.ToInt32(reader.ReadBytes(4).Reverse().ToArray(), 0);
+            int result = BitConverter.ToInt32(Reader.ReadBytes(4).Reverse().ToArray(), 0);
             return result;
         }
 
         public string ReadBytesToHexString(int length)
         {
-            byte[] bytes = reader.ReadBytes(length);
+            byte[] bytes = Reader.ReadBytes(length);
 
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < bytes.Length; i++)
@@ -390,19 +477,18 @@ namespace Org.CNWeak.RyoReader
         }
         public string ReadString(int length)
         {
-            byte[] bytes = reader.ReadBytes(length);
+            byte[] bytes = Reader.ReadBytes(length);
             return Encoding.UTF8.GetString(bytes);
         }
 
-        public bool CheckHasString(string str)
-        {
-            return ReadString(Encoding.UTF8.GetBytes(str).Length) == str;
-        }
+        public bool CheckHasString(string str) => ReadString(Encoding.UTF8.GetBytes(str).Length) == str;
 
         public void Dispose()
         {
-            reader.Dispose();
+            Reader.Dispose();
         }
+
+        public byte[] ReadAllBytes() => Reader.ReadBytes((int)RestLength);
     }
 
     class OutputStreamWriter { }
@@ -426,17 +512,21 @@ namespace Org.CNWeak.RyoReader
         {
             try
             {
-                using (var inputStream = new MemoryStream(input, offset, length))
-                using (var outputStream = new MemoryStream())
-                using (var deflateStream = new DeflateStream(inputStream, CompressionMode.Decompress))
+                var outStream = new MemoryStream();
+                var inflater = new Inflater();
+                inflater.SetInput(input, offset, length);
+                var buffer = new byte[1024];
+                while (!inflater.IsFinished)
                 {
-                    deflateStream.CopyTo(outputStream);
-                    return outputStream.ToArray();
+                    int count = inflater.Inflate(buffer);
+                    outStream.Write(buffer, 0, count);
                 }
+
+                return outStream.ToArray();
             }
             catch (Exception e)
             {
-                PrintError(null,"解压出错",e);
+                PrintError(null, "解压出错", e);
                 return null;
             }
         }
